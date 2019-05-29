aa5d799eeecc6fde8ae8a624390eb1b405750cb0
Dddota <albertliuy@foxmail.com>
2019-05-29 18:45:16 +0800

    Provider P79

---
+++ src/Provider.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+import {PropTypes} from 'prop-types';
+
+class Provider extends Component{
+    getChildContext(){
+        return{
+            store:this.props.store
+        };
+    }
+    render(){
+        return this.props.children;
+    }
+}
